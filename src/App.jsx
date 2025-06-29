c58d591372812da8040a179fcbfc6fb6b524205b
krishnash648 <128827476+krishnash648@users.noreply.github.com>
2025-06-29 11:43:19 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,9 +47,8 @@ const App = () => {
           </div>
         </div>
       </div>
-      {activeSong && (
+      {(activeSong?.title || activeSong?.attributes?.name) && (
         <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          {console.log("🎵 Rendering MusicPlayer", activeSong)}
           <MusicPlayer />
         </div>
       )}
